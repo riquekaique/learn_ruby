26375bd185edad6a4b6e7c37f3c362ee9aeae3b9
Kaique Borges <rique.kaique@gmail.com>
2016-10-10 22:09:21 -0300

    Solved challenge 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
 #write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+  sum = 0
+  array.each do |number|
+    sum += number
+  end
+  sum
+end
+
+def multiply(array)
+  product = 1
+  array.each do |number|
+    product *= number
+  end
+  product
+end
+
+def power(a, b)
+  a ** b
+end
+
+def factorial(n)
+  result = 1
+  n.times do |i|
+    result *= (n-i)
+  end
+  result
+end
