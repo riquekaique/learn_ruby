2fe74834fc4464f180bdd2e66bcec10b7a90f5af
Kaique Borges <rique.kaique@gmail.com>
2016-10-12 18:47:13 -0300

    Finished challenge 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title
+    @title = @title.split
+    little_words = %w(and or if the in of over a an)
+    @title.each do |word|
+      unless little_words.include?(word)
+        word.capitalize!
+      end
+    end
+    @title[0].capitalize!
+    @title.join(" ")
+  end
+
 end
